3e4a56cf1d0533cba56fa20a5ff1c3a1297bd32c
Jiren0 <adeildodeido9@gmail.com>
2025-05-08 09:23:19 -0300

    commit Inicial

--- juros.java
+++ juros.java
@@ -1,3 +1,6 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class juros {
 
     public static void main(String[] args) {
@@ -5,5 +8,12 @@ public class juros {
         //Declaração de variavel
         double Juros, Capital, Taxa, Prazo;
         int Tempo;
+
+        //Criação e instância do objeto de entrada e decimal
+        Scanner entrada = new Scanner (System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        //Apresentação
+        System.out.println("\n\t\t\t -- Calculadora de Juros Simples --\n");
     }
 }
